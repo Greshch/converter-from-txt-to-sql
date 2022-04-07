882d44c0f1f57b91b7462bdf298e48f61ae4a813
Greshch <greshchook@gmail.com>
2022-04-07 11:33:16 +0300

    INSERT without id

--- main.cpp
+++ main.cpp
@@ -1,4 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+int const SIZE = 255;
+
 int main()
 {
+    string temp;
+    ifstream reader("a5.txt"); // открываем файл для чтения
+    string left = "INSERT INTO A2(text, id) VALUES('";
+    string midle = "', ";
+    string right = " );";
+    int les_id = 5;
+    string id_s= to_string(les_id);
+    while (true)
+    {
+        if (getline(reader, temp))
+        {
+            if (temp.size())
+            {
+                cout << left + temp + midle + id_s + right  << endl;
+            }
+
+        }
+        else
+        {
+            break;
+        }
+    }
+    reader.close();
     return 0;
 }
